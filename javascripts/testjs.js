2a178b0774f8d4cc8abaf01da283cfda15a7d969
warispon <warispon0505@gmail.com>
2025-10-02 17:23:46 +0700

    ass1

--- javascripts/testjs.js
+++ javascripts/testjs.js
@@ -1,14 +1,12 @@
-// รับค่าจากผู้ใช้
+
 let input = prompt("Input integer number");
 
-// แปลงเป็น Number
 let number = Number(input);
 
-// ตรวจสอบว่าเป็นจำนวนเต็มหรือไม่
 if (!Number.isInteger(number)) {
   alert("Invalid input! Please enter an integer number only.");
 } else {
-  // ตรวจสอบว่าเป็นศูนย์ เลขคู่ หรือเลขคี่
+ 
   if (number === 0) {
     alert("zero integer number");
   } else if (number % 2 === 0) {
